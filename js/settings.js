f03e6345c08686eaeea0a8b6f5e43d9f8ab51b5c
Samy <152478286+SamyGoumiri@users.noreply.github.com>
2025-02-22 00:42:32 +0100

    e

--- js/settings.js
+++ js/settings.js
@@ -3,6 +3,5 @@ document.getElementById('theme-select').addEventListener('change', function() {
 });
 
 document.getElementById('language-select').addEventListener('change', function() {
-    // Logic to change the language of the site
     alert('Language changed to ' + this.value);
 });
