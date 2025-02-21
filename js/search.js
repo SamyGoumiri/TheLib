4aeae7026df5a57676e99a53a72d95389713b2f1
Samy <152478286+SamyGoumiri@users.noreply.github.com>
2025-02-22 00:39:26 +0100

    r

--- js/search.js
+++ js/search.js
@@ -3,19 +3,23 @@ function searchItems() {
     const filter = searchInput.value.toLowerCase();
     const tables = document.getElementsByTagName('table');
 
-    for (let table of tables) {
-        const rows = table.getElementsByTagName('tr');
-        
-        for (let row of rows) {
-            const cells = row.getElementsByTagName('td');
-            if (cells.length) {
-                const text = cells[0].textContent || cells[0].innerText;
-                if (text.toLowerCase().indexOf(filter) > -1) {
-                    row.style.display = '';
-                } else {
-                    row.style.display = 'none';
+    let timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+        for (let table of tables) {
+            const rows = table.getElementsByTagName('tr');
+            
+            for (let row of rows) {
+                const cells = row.getElementsByTagName('td');
+                if (cells.length) {
+                    const text = cells[0].textContent || cells[0].innerText;
+                    if (text.toLowerCase().indexOf(filter) > -1) {
+                        row.style.display = '';
+                    } else {
+                        row.style.display = 'none';
+                    }
                 }
             }
         }
-    }
+    }, 300);
 }
