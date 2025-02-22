02f10b58e8f2dbaec55770c44f99fb98d68ebae3
Samy <152478286+SamyGoumiri@users.noreply.github.com>
2025-02-22 22:28:55 +0100

    3

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const IGDB_CONFIG = {
-    CLIENT_ID: 'votre_client_id',
-    CLIENT_SECRET: 'votre_client_secret',
+    CLIENT_ID: 'votre_client_id_twitch',
+    CLIENT_SECRET: 'votre_client_secret_twitch',
     ACCESS_TOKEN: '',
     API_URL: 'https://api.igdb.com/v4'
 };
